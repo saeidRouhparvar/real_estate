a8bd2df0062c59f397b9e29484e798c714c0574b
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-16 00:17:13 +0330

    create typography

---
+++ component/Typography.tsx
@@ -0,0 +1,14 @@
+const Typography = ({
+    H1:{fontFamily:"Kufam",fontWeight:700 , fontSize: "45px", lineHeight: "49.5px", fontStyle:'normal'},
+    H2:{fontFamily:"Kufam",fontWeight:700 , fontSize: "40px", lineHeight: "44px", fontStyle:'normal'},
+    H3:{fontFamily:"Kufam",fontWeight: 700, fontSize: "35px", lineHeight: "38.5px", fontStyle:'normal'},
+    H4:{fontFamily:"Kufam",fontWeight: 700, fontSize: "30px", lineHeight: "33px", fontStyle:'normal'},
+    H5:{fontFamily:"Kufam",fontWeight: 700, fontSize: "25px", lineHeight: "27.5px", fontStyle:'normal'},
+    H6:{fontFamily:"Kufam",fontWeight: 700, fontSize: "22px", lineHeight: "20px", fontStyle:'normal'},
+    P:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "20px", lineHeight: "31.2px", fontStyle:'normal'},
+    List:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "20px", lineHeight: "40.4px", fontStyle:'normal'},
+    Blockquote:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "30px", lineHeight: "35.55px", fontStyle:'italic'},
+    Button:{fontFamily:"SourceSans3",fontWeight: 500, fontSize: "20px", lineHeight: "23.7px", fontStyle:'normal'},
+})
+
+export default {...Typography}
