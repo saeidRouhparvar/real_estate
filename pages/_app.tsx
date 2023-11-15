57d760ed31c4cedcfe6d3a0ab5d835765e6f38d8
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-16 00:18:32 +0330

    add theme to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import "@/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import {  ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-  (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-  )
+  return (
+    <ChakraProvider theme={theme} >
+      <Component  {...pageProps} />
+    </ChakraProvider>
+      
+  );
 }
