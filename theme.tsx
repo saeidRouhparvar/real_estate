9c1a62d46021080842371930e99e5d7442f85e3b
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-17 23:48:27 +0330

    refactor theme

--- theme.tsx
+++ theme.tsx
@@ -3,15 +3,19 @@ import ButtonStyle from "./component/Style/ButtonStyle";
 
 const theme = extendTheme({
   colors: {
-    ColorWhite:"#FFFFFF",
-    ColorPrimary: "#3B3A5D",
-    SecondaryColor: "#FFBB50",
-    ContentColor: "#8F8F8F",
-    LinkColor: "#B16642",
-    BorderColor: "#B16642",
+    Color: {
+      ColorWhite: "#FFFFFF",
+      ColorPrimary: "#3B3A5D",
+      SecondaryColor: "#FFBB50",
+      ContentColor: "#8F8F8F",
+      LinkColor: "#B16642",
+      BorderColor: "#B16642",
+      TextColor: "#0A0B3B",
+      TextColor2: "#A4A4A4",
+    },
   },
-  components:{
-   Button:ButtonStyle
-  }
+  components: {
+    Button: ButtonStyle,
+  },
 });
-export default theme
+export default theme;
