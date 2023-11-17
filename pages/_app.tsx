630e38cbb863abcd084aab5703546267d730092d
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-17 23:48:57 +0330

    add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import {  ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import Navbar from "@/component/Navbar";
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} >
+      <Navbar/>
       <Component  {...pageProps} />
     </ChakraProvider>
       
