70579ae1b86574161cdb49aca00ce46490e19b67
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-27 23:46:19 +0330

    done main bnner

--- component/MainBanner.tsx
+++ component/MainBanner.tsx
@@ -1,31 +1,17 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { HomeIcon } from "./svg";
+import { Box, ButtonGroup, Flex, Image } from "@chakra-ui/react";
+import { Banner, HomeIcon, RightArrow } from "./svg";
 import Typography from "./Typography";
 import { pxToRem } from "./Utils";
+import Button from "./form/Button";
+import Home from "../public/img/Img.svg";
+import Container from "./Container";
+import Navbar from "./Navbar";
 
 const MainBanner = () => {
   return (
-    <Flex direction={"column"} gap={pxToRem(38)}>
-      <Flex gap={"4px"}>
-        <Box>{HomeIcon}</Box>
-        <Box
-          px={pxToRem(9)}
-          {...Typography.P}
-          color={`Color.LinkColor`}
-          bg={
-            "linear-gradient(270deg, #F3D1C1 -3.75%, rgba(255, 178, 141, 0.00) 108.58%)"
-          }
-        >
-          Real Estate Agency
-        </Box>
-      </Flex>
-
-      <Flex direction={"column"} gap={pxToRem(10)} {...Typography.MainH} color={`Color.ColorPrimary`}>
-        <Box>Find the perfect </Box>
-        <Box>place to Live with</Box>
-        <Box> your family</Box>
-      </Flex>
-      <Box color={`Color.LinkColor`}>Distinctively re-engineer revolutionary meta-services and premium <br/> architectures. Intrinsically incubate.</Box>
+    <Flex alignItems={"center"} zIndex={-1} h={"990px"} justify={"center"}> 
+      {Banner}
+      <Navbar />
     </Flex>
   );
 };
