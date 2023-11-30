0200e0fdfb0c45d6c185c582ebbaccac606572f5
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-30 22:06:35 +0330

    add new typography

--- component/Typography.tsx
+++ component/Typography.tsx
@@ -1,7 +1,8 @@
 const Typography = ({
+    MainH :{fontFamily:"Kufam",fontWeight:700 , fontSize: "65px", lineHeight: "77px", fontStyle:'normal'},
     H1:{fontFamily:"Kufam",fontWeight:700 , fontSize: "45px", lineHeight: "49.5px", fontStyle:'normal'},
     H2:{fontFamily:"Kufam",fontWeight:700 , fontSize: "40px", lineHeight: "44px", fontStyle:'normal'},
-    H3:{fontFamily:"Kufam",fontWeight: 700, fontSize: "35px", lineHeight: "38.5px", fontStyle:'normal'},
+    H3:{fontFamily:"Inter",fontWeight: 700, fontSize: "35px", lineHeight: "38.5px", fontStyle:'normal'},
     H4:{fontFamily:"Kufam",fontWeight: 700, fontSize: "30px", lineHeight: "33px", fontStyle:'normal'},
     H5:{fontFamily:"Kufam",fontWeight: 700, fontSize: "25px", lineHeight: "27.5px", fontStyle:'normal'},
     H6:{fontFamily:"Kufam",fontWeight: 700, fontSize: "22px", lineHeight: "20px", fontStyle:'normal'},
