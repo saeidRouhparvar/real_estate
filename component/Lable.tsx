3d215bb805aa5987dad75c0facba2403a24cfa29
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-30 22:06:53 +0330

    create label

--- component/Lable.tsx
+++ component/Lable.tsx
@@ -1,4 +1,5 @@
 import { Box, BoxProps, Flex } from "@chakra-ui/react";
+import Typography from "./Typography";
 
 interface Interface extends BoxProps {
   reset?: any;
@@ -12,11 +13,12 @@ const Label = ({ logo, title, ...reset }: Interface) => {
       border={"1px solid #D7D7D7"}
       padding={"14px 12px"}
       borderRadius={"8px"}
+      align={'center'} gap={'10px'} 
       {...reset}
     >
       <Box>{logo}</Box>
       <Box>|</Box>
-      <Box>{title}</Box>
+      <Box color={`Color.ContentColor4`} {...Typography.P}>{title}</Box>
     </Flex>
   );
 };
