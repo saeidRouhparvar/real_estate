d5d0fe4cb29a8055302c46e5937b3c5f4786c9b6
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-02 23:53:36 +0330

    add new color

--- theme.tsx
+++ theme.tsx
@@ -9,6 +9,8 @@ const theme = extendTheme({
       SecondaryColor: "#FFBB50",
       ContentColor1: "#8F8F8F",
       ContentColor2: "#F2F2F2",
+      ContentColor3: "#F5F1EF", 
+      ContentColor4: "#8E8E8E", 
       LinkColor: "#B16642",
       BorderColor: "#B16642",
       TextColor: "#0A0B3B",
