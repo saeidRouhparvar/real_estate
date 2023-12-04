53703e5f2d3fceb2e84e40c4052479a5afd350e7
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-04 22:49:56 +0330

    create main layout

---
+++ layout/mainLayout.tsx
@@ -0,0 +1,42 @@
+import {useHeader} from "@/context/headerContext";
+import {useMemo} from "react";
+import {Box} from "@chakra-ui/react";
+
+const MainLayout = ({children}: any) => {
+    const {header, type} = useHeader()
+
+    const HeaderImage = useMemo(()=>{
+        return  <Box backgroundImage={header} width={"full"} height={type === "main"?"200px":"700px"} backgroundRepeat={"no-repeat"} backgroundSize={"cover"}/>
+
+    },[header,type])
+
+    const HeaderContent = useMemo(() => {
+        if (type === "main") {
+            return <>
+                {HeaderImage}
+            </>
+        }
+        if (type === "big") {
+            return <>
+                {HeaderImage}
+            </>
+        }
+        if (type === "withContact") {
+            return <>
+                {HeaderImage}
+            </>
+        }
+        return <></>
+    }, [type,HeaderImage])
+
+    return <>
+        {HeaderContent}
+
+
+
+
+        {children}
+    </>
+}
+
+export default MainLayout
