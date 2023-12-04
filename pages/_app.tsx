a9810b528d68c7215a30869ef860ab567f6dbb19
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-04 22:51:23 +0330

    add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import {  ChakraProvider } from "@chakra-ui/react";
+import type {AppProps} from "next/app";
+import {ChakraProvider} from "@chakra-ui/react";
 import theme from "../theme";
-import Navbar from "@/component/Navbar";
+import {HeaderProvider} from "@/context/headerContext";
+import MainLayout from "@/layout/mainLayout";
 
 
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component  {...pageProps} />
-    </ChakraProvider>
-      
-  );
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ChakraProvider theme={theme}>
+            <HeaderProvider>
+                <MainLayout>
+                    <Component  {...pageProps} />
+                </MainLayout>
+            </HeaderProvider>
+        </ChakraProvider>
+    );
 }
