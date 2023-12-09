2d613b28391628f558714422ac437dd1c310ac22
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-09 22:46:19 +0330

    refactor header context

--- context/headerContext.tsx
+++ context/headerContext.tsx
@@ -1,54 +1,50 @@
-import {createContext, useContext, useEffect, useState} from "react";
-
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface type {
-    type?: "main" | "big" | "withContact"
+  type?: "main" | "big" | "withContact";
 }
 
 interface header {
-    header?: any
-
+  header?: any;
 }
 
 interface Interface extends type, header {
-    setHeader: (data: any) => void
-    setType: (data: "main" | "big" | "withContact") => void
+  setHeader: (data: any) => void;
+  setType: (data: "main" | "big" | "withContact") => void;
 }
 
-interface inputInterface extends type, header {
+interface inputInterface extends type, header {}
 
-}
+const HeaderContext = createContext<any>(undefined);
 
-const HeaderContext = createContext<any>(undefined)
-
-
-const HeaderProvider = ({children}: any) => {
-    const [header, setHeader] = useState(null)
-    const [type, setType] = useState("main")
-    return <HeaderContext.Provider value={{header, setHeader, type, setType}}>{children}</HeaderContext.Provider>
-}
+const HeaderProvider = ({ children }: any) => {
+  const [header, setHeader] = useState(null);
+  const [type, setType] = useState("main");
+  return (
+    <HeaderContext.Provider value={{ header, setHeader, type, setType }}>
+      {children}
+    </HeaderContext.Provider>
+  );
+};
 
 const useHeader = (prop?: inputInterface) => {
-    const value: Interface = useContext(HeaderContext)
-    if (value === undefined) {
-        console.log("داداش پرووایدر هانی")
+  const value: Interface = useContext(HeaderContext);
+  if (value === undefined) {
+  }
+  const { type, header }: any = prop || { type: undefined, header: undefined };
+
+  useEffect(() => {
+    if (type != undefined) {
+      const { setType } = value;
+      setType(type);
     }
-    const {type, header}: any = prop || {type:undefined, header:undefined}
+    if (header != undefined) {
+      const { setHeader } = value;
+      setHeader(header);
+    }
+  }, [type, header]);
 
-    useEffect(() => {
+  return value;
+};
 
-        if (type != undefined) {
-            const {setType} = value
-            setType(type)
-        }
-        if (header != undefined) {
-            const {setHeader} = value
-            setHeader(header)
-        }
-    }, [type])
-
-    return value
-
-}
-
-export {HeaderProvider, useHeader}
+export { HeaderProvider, useHeader };
