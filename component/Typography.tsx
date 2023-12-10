8d39c1a7c076971c735b15cf5a61c578462cdab2
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-10 23:56:14 +0330

    add new typography

--- component/Typography.tsx
+++ component/Typography.tsx
@@ -7,6 +7,7 @@ const Typography = ({
     H5:{fontFamily:"Kufam",fontWeight: 700, fontSize: "25px", lineHeight: "27.5px", fontStyle:'normal'},
     H6:{fontFamily:"Kufam",fontWeight: 700, fontSize: "22px", lineHeight: "20px", fontStyle:'normal'},
     P:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "20px", lineHeight: "31.2px", fontStyle:'normal'},
+    Box:{fontFamily:"SourceSans3",fontWeight: 600, fontSize: "25px", lineHeight: "31.2px", fontStyle:'normal'},
     List:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "20px", lineHeight: "40.4px", fontStyle:'normal'},
     Blockquote:{fontFamily:"SourceSans3",fontWeight: 400, fontSize: "30px", lineHeight: "35.55px", fontStyle:'italic'},
     Button:{fontFamily:"SourceSans3",fontWeight: 500, fontSize: "20px", lineHeight: "23.7px", fontStyle:'normal'},
