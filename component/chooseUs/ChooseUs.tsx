069aa3a182bbcbdcf85db65854c55fadbed2d57e
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-11 23:31:23 +0330

    create choose us

--- component/chooseUs/ChooseUs.tsx
+++ component/chooseUs/ChooseUs.tsx
@@ -1,9 +1,10 @@
-import { Box, Flex, GridItem, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import MiniTitle from "../miniTitle";
 import Typography from "../Typography";
 import { pxToRem } from "../Utils";
 import Container from "../Container";
 import ChooseBox from "./ChooseBox";
+import { Insurance, Piggy, PiggyTop } from "../svg";
 
 const ChooseUs = () => {
   return (
@@ -19,16 +20,73 @@ const ChooseUs = () => {
         <Box>Of Real Estate Industries</Box>
       </Flex>
 
-      <Container>
-        <SimpleGrid columns={3}>
-          <GridItem>
-            <ChooseBox/>
-          </GridItem>
-          <GridItem>sadsdsa</GridItem>
-          <GridItem>sadsdsa</GridItem>
-          <GridItem>sadsdsa</GridItem>
-          <GridItem>sadsdsa</GridItem>
-        </SimpleGrid>
+      <Container >
+        <Flex direction={"column"} gap={pxToRem(33)} mt={pxToRem(46)}>
+          <Flex gap={pxToRem(38)}>
+            <ChooseBox 
+              title="Budget Friendly"
+              description="Distinctively re-engineer 
+              revolutionary meta-services and premium
+                At vero eos et accusamus et iusto odio
+                 dignissimos ducimus qui blanditiis"
+              imgBig={Piggy}
+              imgChild={PiggyTop}
+            />
+            <Flex
+              bg={"linear-gradient(143deg, #F3D1C1 2.44%, #FFEEE5 98.38%)"}
+              padding={"48px 41px"}
+              align={"center"}
+            >
+              <Box>{Insurance}</Box>
+              <Flex direction={"column"}>
+                <Box {...Typography.Box} color={`Color.ColorPrimary`}>
+                  Property Insurance
+                </Box>
+                <Flex direction={"column"} gap={pxToRem(30)}>
+                  <Box>
+                    Distinctively re-engineer revolutionary meta-services and
+                    premium At vero eos et accusamus et iusto odio dignissimos
+                    ducimus qui blanditiis
+                  </Box>
+                  <Box>
+                    Distinctively re-engineer revolutionary meta-services and
+                    premium.
+                  </Box>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <Flex gap={pxToRem(38)}>
+            <ChooseBox
+              title="Budget Friendly"
+              description="Distinctively re-engineer 
+              revolutionary meta-services and premium
+                At vero eos et accusamus et iusto odio
+                 dignissimos ducimus qui blanditiis"
+              imgBig={Piggy}
+              imgChild={PiggyTop}
+            />
+            <ChooseBox
+              title="Budget Friendly"
+              description="Distinctively re-engineer 
+            revolutionary meta-services and premium
+              At vero eos et accusamus et iusto odio
+               dignissimos ducimus qui blanditiis"
+              imgBig={Piggy}
+              imgChild={PiggyTop}
+            />
+            <ChooseBox
+              title="Budget Friendly"
+              description="Distinctively re-engineer 
+          revolutionary meta-services and premium
+            At vero eos et accusamus et iusto odio
+             dignissimos ducimus qui blanditiis"
+              imgBig={Piggy}
+              imgChild={PiggyTop}
+            />
+          </Flex>
+        </Flex>
       </Container>
     </Flex>
   );
