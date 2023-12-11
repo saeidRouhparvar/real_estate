fa04f5b68e49c32254ab691689bf064227e02ef7
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-11 23:31:52 +0330

    main layout refactor

--- layout/mainLayout.tsx
+++ layout/mainLayout.tsx
@@ -1,42 +1,42 @@
-import {useHeader} from "@/context/headerContext";
-import {useMemo} from "react";
-import {Box} from "@chakra-ui/react";
+import { useHeader } from "@/context/headerContext";
+import { useMemo } from "react";
+import { Box } from "@chakra-ui/react";
 
-const MainLayout = ({children}: any) => {
-    const {header, type} = useHeader()
+const MainLayout = ({ children }: any) => {
+  const { header, type } = useHeader();
 
-    const HeaderImage = useMemo(()=>{
-        return  <Box backgroundImage={header} width={"full"} height={type === "main"?"200px":"700px"} backgroundRepeat={"no-repeat"} backgroundSize={"cover"}/>
+  const HeaderImage = useMemo(() => {
+    return (
+      <Box
+        backgroundImage={header}
+        width={"full"}
+        height={type === "main" ? "721px" : "996px"}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"cover"}
+      />
+    );
+  }, [header, type]);
 
-    },[header,type])
+  const HeaderContent = useMemo(() => {
+    if (type === "main") {
+      return <>{HeaderImage}</>;
+    }
+    if (type === "big") {
+      return <>{HeaderImage}</>;
+    }
+    if (type === "withContact") {
+      return <>{HeaderImage}</>;
+    }
+    return <></>;
+  }, [type, HeaderImage]);
 
-    const HeaderContent = useMemo(() => {
-        if (type === "main") {
-            return <>
-                {HeaderImage}
-            </>
-        }
-        if (type === "big") {
-            return <>
-                {HeaderImage}
-            </>
-        }
-        if (type === "withContact") {
-            return <>
-                {HeaderImage}
-            </>
-        }
-        return <></>
-    }, [type,HeaderImage])
+  return (
+    <>
+      {HeaderContent}
 
-    return <>
-        {HeaderContent}
-
-
-
-
-        {children}
+      {children}
     </>
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
