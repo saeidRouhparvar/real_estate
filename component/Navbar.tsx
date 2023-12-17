d880556ed99875129d0aedc2628e97836eb26010
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-12-17 23:09:18 +0330

    refactor navbar

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -2,38 +2,56 @@ import { Box, BoxProps, Flex } from "@chakra-ui/react";
 import { pxToRem } from "./Utils";
 import Typography from "./Typography";
 import { Logo } from "./svg";
+import Container from "./Container";
 
 const Navbar = () => {
   return (
-    <Box px={pxToRem(261)} mt={pxToRem(100)}>
-      <Flex p={pxToRem(45)} bg={`Color.ColorWhite`} justify={"space-between"} align={"center"}>
-        <Flex align={"center"} gap={"6px"}>
-            <Box>{Logo}</Box>
-            <Box mt={pxToRem(30)} {...Typography.H3}>ProperLand</Box>
-        </Flex>
+    <Flex
+      position={"absolute"}
+      top={pxToRem(98)}
+      zIndex={1000000}
+      left={0}
+      right={0}
+      justify={"center"}
+      align={"center"}
+    >
+      <Container>
         <Flex
-          {...Typography.P}
-          color={`Color.TextColor2`}
+          p={pxToRem(45)}
+          bg={`Color.ColorWhite`}
+          justify={"space-between"}
           align={"center"}
-          gap={pxToRem(20)}
         >
-          <Box color={`Color.TextColor`}>Home</Box>
-          <Box>about</Box>
-          <Box>Properties</Box>
-          <Box>Blog</Box>
-          <Box
-            bg={`Color.ColorPrimary`}
-            px={pxToRem(40)}
-            py={pxToRem(16)}
-            borderRadius={"5px"}
-            color={`Color.ColorWhite`}
-            ml={pxToRem(10)}
+          <Flex align={"center"} gap={"6px"}>
+            <Box>{Logo}</Box>
+            <Box mt={pxToRem(30)} {...Typography.H3}>
+              ProperLand
+            </Box>
+          </Flex>
+          <Flex
+            {...Typography.P}
+            color={`Color.TextColor2`}
+            align={"center"}
+            gap={pxToRem(20)}
           >
-            Contact
-          </Box>
+            <Box color={`Color.TextColor`}>Home</Box>
+            <Box>about</Box>
+            <Box>Properties</Box>
+            <Box>Blog</Box>
+            <Box
+              bg={`Color.ColorPrimary`}
+              px={pxToRem(40)}
+              py={pxToRem(16)}
+              borderRadius={"5px"}
+              color={`Color.ColorWhite`}
+              ml={pxToRem(10)}
+            >
+              Contact
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Container>
+    </Flex>
   );
 };
 export default Navbar;
